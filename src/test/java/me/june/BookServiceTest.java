e5ccc42dbf97fd68acbf572be5384a0e232380a0
ces518 <pupupee9@gmail.com>
2019-11-11 23:18:29 +0900

    bytebuddy proxy

--- src/test/java/me/june/BookServiceTest.java
+++ src/test/java/me/june/BookServiceTest.java
@@ -1,13 +1,16 @@
 package me.june;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import org.junit.Test;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import java.lang.reflect.*;
+import java.nio.ByteBuffer;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,7 +52,7 @@ public class BookServiceTest {
     // 반드시 인터페이스 여야한다.
 
     @Test
-    public void di () {
+    public void di () throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         // invocation handler 를 넘겨준것처럼 handler를 넘겨주어야 한다.
         // 이전의 다이나믹 프록시와 마찬가지로 프록시 객체가 부가적인 일을 할 부분을 구현해준다.
         MethodInterceptor handler = new MethodInterceptor() {
@@ -66,5 +69,23 @@ public class BookServiceTest {
         };
         // Enhancer 가 CGlib의 핵심 클래스이다.
         BookService bookService = (BookService) Enhancer.create(BookService.class, handler);
+
+        // bytebuddy 를 사용한 방법
+        // 인스턴스를 바로 만들어주지 않고, 클래스를 만들어 주어야한다.
+        // 프록시 클래스를 만드는것과 비슷한다.
+        Class<? extends BookService> proxyClass = new ByteBuddy().subclass(BookService.class)
+                // rent method를 선택하여 InvocationHandler에게 위임한다.
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+                    BookService bookService = new DefaultBookService();
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        Object invoke = method.invoke(bookService, args);
+                        return invoke;
+                    }
+                }))
+                .make().load(BookService.class.getClassLoader()).getLoaded();
+
+        // 아무런 것도 변경되지 않은 하위클래스를 생성
+        BookService bookService2 = proxyClass.getConstructor(null).newInstance();
     }
 }
